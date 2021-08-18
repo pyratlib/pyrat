67b5397ae34346a3997bba3b095cfb9508374efe
Tulio F. Almeida <54067432+tuliofalmeida@users.noreply.github.com>
2021-08-18 09:33:10 -0300

    pyrat v0.1.5

--- pyratlib/processing.py
+++ pyratlib/processing.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib import cm
 
-def Trajectory (data, bodyPartTraj, bodyPartBox, cmapType = 'viridis', figureTitle = None, 
-              hSize=6, wSize =8,fontsize=15, invertY = True, saveName=None, figformat =".eps"):
+def Trajectory (data,bodyPartTraj,bodyPartBox,cmapType='viridis',figureTitle=None, 
+                hSize=6,wSize=8,fontsize=15,invertY=True,saveName=None,figformat=".eps"):
     """
     Plots the trajectory of the determined body part.
 
@@ -49,6 +49,10 @@ def Trajectory (data, bodyPartTraj, bodyPartBox, cmapType = 'viridis', figureTit
     -----
     This function was developed based on DLC outputs and is able to support 
     matplotlib configurations."""
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    from matplotlib import cm
     values = (data.iloc[2:,1:].values).astype(np.float)
     lista1 = (data.iloc[0][1:].values +" - " + data.iloc[1][1:].values).tolist()
 
@@ -90,8 +94,8 @@ def Trajectory (data, bodyPartTraj, bodyPartBox, cmapType = 'viridis', figureTit
     plt.show()
 
 
-def Heatmap(data, bodyPart, cmapType = 'viridis', figureTitle = None, hSize=6, wSize =8,
-            bins = 40, vmax= 1000, fontsize=15, invertY = True, saveName=None, figformat = ".eps"):
+def Heatmap(data,bodyPart,cmapType='viridis',figureTitle=None,hSize=6,wSize =8,
+            bins=40,vmax=1000,fontsize=15,invertY=True,saveName=None,figformat=".eps"):
     """
     Plots the trajectory heatmap of the determined body part.
 
@@ -137,6 +141,11 @@ def Heatmap(data, bodyPart, cmapType = 'viridis', figureTitle = None, hSize=6, w
     -----
     This function was developed based on DLC outputs and is able to support 
     matplotlib configurations."""
+    import numpy as np
+    import matplotlib.pyplot as plt
+    import pandas as pd
+    from matplotlib import cm
+
     values = (data.iloc[2:,1:].values).astype(np.float)
     lista1 = (data.iloc[0][1:].values +" - " + data.iloc[1][1:].values).tolist()
 
@@ -196,7 +205,7 @@ def ScaleConverter(dado, pixel_max,pixel_min,max_real, min_real=0):
 
     return min_real + ((dado-pixel_min)/(pixel_max-pixel_min)) * (max_real-min_real)
 
-def MotionMetrics (data, bodyPart, filter=0, fps=30, max_real = 60, min_real = 0):
+def MotionMetrics (data,bodyPart,filter=0,fps=30,max_real=60,min_real=0):
     """
     Performs motion-related metrics such as velocity, acceleration, and distance.
 
@@ -206,7 +215,7 @@ def MotionMetrics (data, bodyPart, filter=0, fps=30, max_real = 60, min_real = 0
         The input tracking data.
     bodyPart : str
         Body part you want use as reference.
-    filter : int
+    filter : float
         Threshold to remove motion artifacts. Adjust according to the tracking 
         quality and speed of what is moving.   
     fps : int
@@ -229,6 +238,8 @@ def MotionMetrics (data, bodyPart, filter=0, fps=30, max_real = 60, min_real = 0
     -----
     This function was developed based on DLC outputs and is able to support 
     matplotlib configurations."""
+    import numpy as np
+    import pandas as pd
 
     values = (data.iloc[2:,1:].values).astype(np.float)
     lista1 = (data.iloc[0][1:].values +" - " + data.iloc[1][1:].values).tolist()
@@ -250,8 +261,7 @@ def MotionMetrics (data, bodyPart, filter=0, fps=30, max_real = 60, min_real = 0
 
     return df
 
-
-def createObjectDf(Fields):
+def FieldDetermination(Fields):
     """
     Creates a data frame with the desired dimensions to extract information about
     an area. Therefore, you must determine the area in which you want to extract 
@@ -277,8 +287,11 @@ def createObjectDf(Fields):
     This function was developed based on DLC outputs and is able to support 
     matplotlib configurations."""
 
+    import numpy as np
     import pandas as pd
-    objects = pd.DataFrame(columns=['fields','center_x','center_y', 'radius', 'a_x', 'a_y' , 'height', 'width'])
+
+    null = .001
+    fields = pd.DataFrame(columns=['fields','center_x','center_y', 'radius', 'a_x', 'a_y' , 'height', 'width'])
     for i in range(Fields):
         print('Enter the object type '+ str(i+1) + " (0 - circular, 1 - rectangular):")
         objectType = int(input())
@@ -289,7 +302,7 @@ def createObjectDf(Fields):
             centerY = int(input())
             print('Enter the radius value of the field ' + str(i+1) + ':')
             radius = int(input())
-            df2 = pd.DataFrame([[objectType, centerX, centerY,radius,'null','null','null','null']], columns=['fields','center_x','center_y', 'radius', 'a_x', 'a_y' , 'height', 'width'])
+            df2 = pd.DataFrame([[objectType, centerX, centerY,radius,null,null,null,null]], columns=['fields','center_x','center_y', 'radius', 'a_x', 'a_y' , 'height', 'width'])
         else:
             print('Enter the X value of the field\'s lower left vertex ' + str(i+1) + ':')
             aX = int(input())
@@ -299,7 +312,146 @@ def createObjectDf(Fields):
             height = int(input())
             print('Enter the field\'s width value ' + str(i+1) + ':')
             width = int(input())
-            df2 = pd.DataFrame([[objectType, 'null','null' , 'null' ,aX,aY,height,width]], columns=['fields','center_x','center_y', 'radius', 'a_x', 'a_y' , 'height', 'width'])
-        objects = objects.append(df2, ignore_index=True)
+            df2 = pd.DataFrame([[objectType, null,null, null ,aX,aY,height,width]], columns=['fields','center_x','center_y', 'radius', 'a_x', 'a_y' , 'height', 'width'])
+        fields = fields.append(df2, ignore_index=True)
         
-    return objects
+    return fields
+
+def Interaction(data,bodyPart,fields):
+    """
+    Performs the metrification of the interaction of the point of the determined 
+    body part and the marked area.
+
+    Parameters
+    ----------
+    data : pandas DataFrame
+        The input tracking data.
+    bodyPart : str
+        Body part you want use as reference.
+    fields : pandas DataFrame
+        The DataFrame with the coordinates of the created fields (output of FieldDetermination()).   
+
+    Returns
+    -------
+    out : int
+        Scale factor of your box.
+
+    See Also
+    --------
+    For more information and usage examples: https://github.com/pyratlib/pyrat
+
+    Notes
+    -----
+    This function was developed based on DLC outputs and is able to support 
+    matplotlib configurations.""" 
+
+    import numpy as np
+    import pandas as pd
+
+    values = (data.iloc[2:,1:].values).astype(np.float)
+    lista1 = (data.iloc[0][1:].values +" - " + data.iloc[1][1:].values).tolist()
+
+    dataX = values[:,lista1.index(bodyPart+" - x")]
+    dataY = values[:,lista1.index(bodyPart+" - y")]
+
+    numObjects = len(fields.index)
+    interact = np.zeros(len(dataX))
+
+    for i in range(len(interact)):
+        for j in range(numObjects):
+            if fields['fields'][0] == 0:
+                if ((dataX[i] - fields['center_x'][j])**2 + (dataY[i] - fields['center_y'][j])**2 <= fields['radius'][j]**2):
+                    interact[i] = j +1
+            else:
+                if fields['a_x'][j] <= dataX[i] <= (fields['a_x'][j] + fields['width'][j]) and fields['a_y'][j] <= dataY[i] <= (fields['a_y'][j] + fields['height'][j]): 
+                    interact[i] = j +1
+        interactsDf = pd.DataFrame(columns=['start','end','obj'])
+
+    obj = 0
+    start = 0
+    end = 0
+    fps =30
+
+    for i in range(len(interact)):
+        if obj != interact[i]:
+            end = ((i-1)/fps)
+            df = pd.DataFrame([[start,end,obj]],columns=['start','end','obj'])
+            obj =  interact[i]
+            start = end
+            interactsDf = interactsDf.append(df, ignore_index=True)
+
+    start = end
+    end = (len(interact)-1)/fps
+    obj = interact[-1]
+    df = pd.DataFrame([[start,end,obj]],columns=['start','end','obj'])
+    interactsDf = interactsDf.append(df, ignore_index=True)
+
+    return interactsDf
+
+def Reports(df_list,list_name,bodypart,fields=None,filter=0.3,fps = 30):
+    """
+    Produces a report of all data passed along the way with movement and interaction metrics in a 
+    given box space.
+
+    Parameters
+    ----------
+    df_list : list
+        List with all DataFrames.
+    list_name : list
+        List with names of each data.
+    bodypart : str
+        Body part you want use as reference.
+    fields : pandas DataFrame
+        The DataFrame with the coordinates of the created fields (output of FieldDetermination()).   
+    filter : float
+        Threshold to remove motion artifacts. Adjust according to the tracking 
+        quality and speed of what is moving.
+    fps : int
+        The recording frames per second.
+
+    Returns
+    -------
+    out : pandas DataFrame
+        DataFrame with report of each data in one line.
+
+    See Also
+    --------
+    For more information and usage examples: https://github.com/pyratlib/pyrat
+
+    Notes
+    -----
+    This function was developed based on DLC outputs and is able to support 
+    matplotlib configurations.""" 
+
+    import numpy as np
+    import pandas as pd
+    import pyratlib as rat
+
+    relatorio = pd.DataFrame(columns=['file','time','dist', 'speed'])
+  
+    if type(fields) != type(None):  
+        for i in range(len(fields)):
+            relatorio["obj{0}".format(i+1)] = []
+            relatorio["time_obj{0}".format(i+1)] = []
+
+    for i,v in enumerate(df_list):
+        lista = [list_name[i]]
+
+        DF = rat.MotionMetrics(df_list[i], bodypart, filter=filter, fps=fps)
+
+        time = DF.Time.iloc[-1]
+        dist = DF.Distance.sum()
+        vMedia = DF.Speed.mean()
+        
+        lista.append(time)
+        lista.append(dist)
+        lista.append(vMedia)
+        if type(fields) != type(None): 
+            interacts = rat.Interaction(df_list[i], bodypart, fields)
+            for i in range(len(fields)):
+                lista.append(interacts["obj"][interacts["obj"] == i+1].count())
+                lista.append((interacts["end"][interacts["obj"] == i+1]-interacts["start"][interacts["obj"] == i+1]).sum())
+        relatorio_temp = pd.DataFrame([lista], columns=relatorio.columns)
+        relatorio = relatorio.append(relatorio_temp, ignore_index=True)
+
+    return relatorio
