4673f6b2278e48eaad9ccd73af19f1c8b13c04c9
Tulio F. Almeida <54067432+tuliofalmeida@users.noreply.github.com>
2022-01-17 10:22:51 -0300

    0.5.4

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
   
 setuptools.setup(
-    name = 'pyratlib',         # How you named your package folder (MyLib)
-    packages = ['pyratlib'],   # Chose the same as "name"
-    version = '0.5.4',      # Start with a small number and increase it with every change you make
-    license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    name = 'pyratlib',         
+    packages = ['pyratlib'],   
+    version = '0.5.4',      
+    license='MIT',       
     description = 'PyRat is a user friendly library in python to analyze data from the DeepLabCut. Developed to help researchers unfamiliar with programming can perform animal behavior analysis more simpler.',   # Give a short description about your library
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url = 'https://github.com/pyratlib/pyrat',   # Provide either the link to your github or to your website
-    download_url = 'https://github.com/pyratlib/pyrat',    # I explain this later on
-    keywords = ['Data analysis', 'Animal Behavior', 'Electrophysiology'],   # Keywords that define your package best
-    install_requires=[            # I get to this in a second
+    url = 'https://github.com/pyratlib/pyrat',  
+    download_url = 'https://github.com/pyratlib/pyrat',    
+    keywords = ['Data analysis', 'Animal Behavior', 'Electrophysiology', 'Tracking', 'DeepLabCut'],   
+    install_requires=[           
             'numpy',
             'pandas',
             'neo',
@@ -23,11 +23,11 @@ setuptools.setup(
             'wheel'
         ],
     classifiers=[
-      'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-      'Intended Audience :: Developers',      # Define that your audience are developers
+      'Development Status :: 4 - Beta',      
+      'Intended Audience :: Developers',      
       'Topic :: Software Development :: Build Tools',
-      'License :: OSI Approved :: MIT License',   # Again, pick a license
-      'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+      'License :: OSI Approved :: MIT License',   
+      'Programming Language :: Python :: 3',      
       'Programming Language :: Python :: 3.4',
       'Programming Language :: Python :: 3.5',
       'Programming Language :: Python :: 3.6',
